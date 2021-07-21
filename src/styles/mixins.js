8997c342f4aeee98d2cf101b0a11c6dde03c53f1
ShayaneCorreia <naalandas@gmail.com>
2021-07-20 22:34:53 -0300

    Changes the Global and Minimal styles page

---
+++ src/styles/mixins.js
@@ -0,0 +1,26 @@
+export const mixins = {
+    colors: {
+        background: "#E4E4E4",
+        primary: "#23A6F0",
+        secondary: "#fff",
+        green: "green",
+        blue: "blue",
+        red: "red",
+        grey: "grey"
+    },
+    fonts: {
+        bold: 'Montserrat-Bold',
+        semi_bold: 'Montserrat-SemiBold',
+        normal: 'Montserrat-Normal',
+    },
+    typograph: {
+        title: "24px",
+        subTitle: "16px",
+        paragraph: "12px"
+    },
+    size: {
+        lg: 240,
+        md: 160,
+        sm: 120
+    }
+}
