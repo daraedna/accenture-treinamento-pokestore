0cf09119b07ce793bc57772143bf8120f67983d6
Caio Ribeiro <caio.ribeiro98@outlook.com>
2021-07-21 21:12:21 -0300

    Add styles.js

--- src/components/Cards/styles.js
+++ src/components/Cards/styles.js
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import {mixins} from "../../styles/mixins"
+
 export const ContainerCard = {
     Container: styled.div `
         background-color: ${mixins.colors};
