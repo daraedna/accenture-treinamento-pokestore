e95fcdd376a592cd5890a489d6171f08f12c9c56
Daraédna Menezes <daraedna@gmail.com>
2021-07-23 12:53:25 -0300

    add component-footer

---
+++ src/components/Footer/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Styles } from './styles';
+
+export default function Footer() {
+    return (
+        <Styles.Footer>
+            <Styles.TextFooter>
+                Todos os direitos reservados © - 2021
+            </Styles.TextFooter>
+        </Styles.Footer>
+    )
+}
---
+++ src/components/Footer/styles.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+import { mixins } from "../../styles/mixins";
+
+export const Styles = {
+    Footer: styled.footer`
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        width: 100%;
+        height: 42px;
+        background: ${mixins.colors.primary};
+    `,
+     TextFooter: styled.p`
+        font-size: ${mixins.typograph.paragraph};
+        color: ${mixins.colors.white};
+    `
+}
