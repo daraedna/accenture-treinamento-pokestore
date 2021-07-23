2ab1a313f6924dd6596bafa4a2ae03c84b23c10a
Lucas Diniz <lucasalvarengadiniz@gmail.com>
2021-07-23 15:54:42 -0300

    first-appearence-cart

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,57 +1,77 @@
 import React from 'react';
-
+import Table from 'react-bootstrap/Table';
+import Button from '../../components/Button';
+import pikachuImg from '../../assets/pikachu.png';
+import { FiTrash, FiPlusCircle, FiMinusCircle } from "react-icons/fi";
 import { Styled } from './styles';
+import { mixins } from "../../styles/mixins";
 
 function Cart() {
   return (
     <Styled.Container>
-      <Styled.CartWrapper>
-        <Styled.CartTable>
-          <thead>
-            <tr>
-              <th scope="col"></th>
-              <th scope="col">Produto</th>
-              <th scope="col">Quantidade</th>
-              <th scope="col">Subtotal</th>
-              <th scope="col"></th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>
-                <img src="" alt="" />
-              </td>
-              <td>
+      <Table
+        borderless
+      >
+        <thead>
+          <tr>
+            <th scope="col"></th>
+            <th scope="col">Produto</th>
+            <th scope="col">Quantidade</th>
+            <th scope="col">Subtotal</th>
+            <th scope="col"></th>
+          </tr>
+        </thead>
+        <tbody>
+          <Styled.ProductRow>
+            <td>
+              <img src={pikachuImg} alt="Imagem do Pokémon" />
+            </td>
+            <td>
+              <Styled.ProductPriceDataCell>
                 <span>Picachu</span>
                 <span>R$ 20.000,00</span>
-              </td>
-              <td>
-                <button>-</button>
-                <input type="text" value="1" />
-                <button>+</button>
-              </td>
-              <td>
-                <span>R$ 20.000,00</span>
-              </td>
-              <td>
-                <button>Lixo</button>
-              </td>
-            </tr>
-          </tbody>
-          <tfoot>
-            <tr>
-              <td colSpan="5">
-                <button>Finzalizar Pedido</button>
-                <div>
+              </Styled.ProductPriceDataCell>
+            </td>
+            <td>
+              <Styled.AmountButton>
+                <FiMinusCircle size="22px" color={mixins.colors.text} />
+              </Styled.AmountButton>
+              <Styled.Input
+                type="number"
+                id="amount"
+                name="amount"
+                value="1"
+                onChange={() => { }} /* trocar para função real */
+              />
+              <Styled.AmountButton>
+                <FiPlusCircle size="22px" color={mixins.colors.text} />
+              </Styled.AmountButton>
+            </td>
+            <td>
+              <span>R$ 20.000,00</span>
+            </td>
+            <td>
+              <Styled.DeleteButton type="button">
+                <FiTrash size="22px" color="#EA4335" />
+              </Styled.DeleteButton>
+            </td>
+          </Styled.ProductRow>
+        </tbody>
+        <tfoot>
+          <tr>
+            <td className="FootRow" colSpan="5">
+              <div className="FootRowContentWrapper">
+                <Button type="primary">Finzalizar Pedido</Button>
+                <div className="totalWrapper">
                   <p>Total: </p>
                   <strong>R$ 38.000,00</strong>
                 </div>
-              </td>
-            </tr>
-          </tfoot>
-        </Styled.CartTable>
-      </Styled.CartWrapper>
-    </Styled.Container>
+              </div>
+            </td>
+          </tr>
+        </tfoot>
+      </Table>
+    </Styled.Container >
   );
 }
 
--- src/pages/Cart/styles.js
+++ src/pages/Cart/styles.js
@@ -8,13 +8,116 @@ export const Styled = {
     background-color: ${mixins.colors.white};
     max-width: 934px;
     display: flex;
-    margin: auto;
-    padding: 2rem;
-  `,
-  CartWrapper: styled.div`
+    align-items: center;
+    justify-content: center;
     margin: 0 auto;
+    padding: 2rem;
+    box-shadow: 5px 6px 10px rgba(0, 0, 0, 0.1);
+
+    table{
+      margin: 0;
+      width: 828px;
+      
+      th {
+        font-size: ${mixins.typograph.title};
+      }
+      
+      th, td {
+        padding: 1rem;
+        /* border: 1px solid blue; */
+        vertical-align: middle;
+        color: ${mixins.colors.text};
+      }
+
+
+      .FootRow{
+        padding: 2rem 0;
+
+        .FootRowContentWrapper{
+          display: flex;
+          align-items: center;
+          justify-content: space-between;
+
+          .totalWrapper {
+            display: flex;
+            align-items: center;
+            justify-content: center;
+
+            p {
+              margin-bottom: 0;
+              margin-right: 0.5rem;
+            }
+          }
+        }
+      }
+    }
+
+    // retirando as arrows do input number
+    /* Chrome, Safari, Edge, Opera */
+    input::-webkit-outer-spin-button,
+    input::-webkit-inner-spin-button {
+      -webkit-appearance: none;
+      margin: 0;
+    }
+    /* Firefox */
+    input[type=number] {
+      -moz-appearance: textfield;
+    }
+    
   `,
-  CartTable: styled.table`
-    empty-cells: show;
+
+  ProductRow: styled.tr`
+    font-size: ${mixins.typograph.subTitle};
+    border-bottom: 1px solid ${mixins.colors.text};
+  `,
+
+  Input: styled.input`
+    text-align: center;
+    height: 36px;
+    width: 44px;
+    padding:0 0.4rem;
+    margin: 0 0.2rem;
+    border: 1px solid #DDDDDD;
+    border-radius: 5px;
+  `,
+
+  AmountButton: styled.button`
+    width: 2rem;
+    height: 2rem;
+    background: none;
+    border: none;
+    border-radius: 5px;
+
+    transition: 0.2s;
+
+    &:hover{
+      background-color: ${mixins.colors.greyHover};
+    }
+
+    &:active{
+      filter: brightness(0.7);
+    }
+  `,
+
+  DeleteButton: styled.button`
+    width: 2rem;
+    height: 2rem;
+    background: none;
+    border: none;
+    border-radius: 5px;
+
+    &:active{
+      background-color: ${mixins.colors.greyHover};
+    }
+  `,
+
+  ProductPriceDataCell: styled.div`
+    display: flex;
+    flex-direction: column;
+    text-align: left;
+    
+    span:first-child {
+      margin-bottom: 1rem;
+    };
   `
 }
