3aa2cb0f852c23cd26361226acd0273be939395c
Caio Ribeiro <caio.ribeiro98@outlook.com>
2021-07-26 15:52:16 -0300

    Add profile components

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,9 +1,32 @@
 import React from 'react';
+import {Styled} from './styles'
+import Footer from '../../components/Footer';
+import profile_img from '../../assets/profile_img.png'
 
-import { Styled } from './styles';
 
-function Profile() {
-  return <div />;
+
+export default function Profile() {
+  return (
+    <Styled.Container>
+      <Styled.Card_Container>
+      <Styled.Content_Container>
+      <Styled.Card_Title>
+        <h2>Meu perfil</h2>
+      </Styled.Card_Title>
+      <Styled.Img src={profile_img}/>
+      </Styled.Content_Container>
+      </Styled.Card_Container>
+      <Footer/>
+    </Styled.Container>
+  );
 }
 
-export default Profile;
+      // <Styled.Content_Container2>
+      //   <Styled.Name>
+      //     <p>Nome completo</p>  
+      //   </Styled.Name>
+      //   <select>
+      //     <option>Masculino</option>
+      //     <option>Feminino</option>
+      //   </select>
+      // </Styled.Content_Container2>
