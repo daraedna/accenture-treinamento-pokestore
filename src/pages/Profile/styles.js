6662581b483f669d75eeddbe7000f87022410ef7
Caio Ribeiro <caio.ribeiro98@outlook.com>
2021-07-30 17:17:42 -0300

    Give a margin on edit button

--- src/pages/Profile/styles.js
+++ src/pages/Profile/styles.js
@@ -23,23 +23,24 @@ export const Styled = {
             font-weight: ${mixins.fonts.normal};
             font-size: ${mixins.typograph.title};
             text-align: center;
+            margin: 10px 0 0 0;
         }
 
-        @media screen and (min-width: 768px) {
+        @media screen and (min-width: 768px) and (max-width: 1023px) {
             background-color: ${mixins.colors.background};
             h1 {
                 font-size: 28px;
             }
         }
         
-        @media screen and (min-width: 1024px) {
+        @media screen and (min-width: 1024px) and (max-width: 1199px) {
             background-color: ${mixins.colors.background};
             h1 {
                 font-size: 32px;
             }
         }
 
-        @media screen and (min-width: 1200px) {
+        @media screen and (min-width: 1200px) and (max-width: 1600px) {
             background-color: ${mixins.colors.background};
             h1 {
                 font-size: 36px;
@@ -47,15 +48,23 @@ export const Styled = {
         }
     `,
     Content: styled.div `
+    width: 100%;
     display: flex;
     flex-direction: column;
     align-items: center;
     justify-content: center;
     padding: 1rem;
+
+    @media screen and (min-width: 1024px) {
+        display: flex;
+        flex-direction: row;
+    }
     `,
     Avatar_Container: styled.div `
         margin: 0 0 1.5rem 0;
-    img {
+        img {
+        border: 1px solid black;
+        border-radius: 100%;
         width: 4rem;
 
         @media screen and (min-width: 768px) {
@@ -64,13 +73,17 @@ export const Styled = {
 
         @media screen and (min-width: 1024px) {
             width: 8rem;
+            position: relative;
+            top: -90px;
+            left: -30px;
         }
     }
     `,
 
     Form: styled.form `
-        display: grid;
-        grid-template-columns: 1fr;
+        width: 100%;
+        display: flex;
+        flex-direction: column;
         gap: 0.7rem;
         
         input {
@@ -106,7 +119,10 @@ export const Styled = {
     }
 
     @media screen and (min-width: 1200px) {
+        display: grid;
+        grid-template-columns: 1fr 1fr;
         input {
+            
             width: 22rem;
         }
         select {
@@ -144,33 +160,33 @@ export const Styled = {
         width: 100%;
     }
     `,
-    Sex_Container: styled.div `
-       width: 100%;
-       display:flex;
-       flex-direction: column;
-       color: ${mixins.colors.text};
-       font-size:${mixins.typograph.form};
-        gap: 0.5rem;
+    // Sex_Container: styled.div `
+    //    width: 100%;
+    //    display:flex;
+    //    flex-direction: column;
+    //    color: ${mixins.colors.text};
+    //    font-size:${mixins.typograph.form};
+    //     gap: 0.5rem;
 
-        @media screen and (min-width: 768px) {
-        label {
-            font-size:16px;
-        }
-    }
+    //     @media screen and (min-width: 768px) {
+    //     label {
+    //         font-size:16px;
+    //     }
+    // }
 
-    @media screen and (min-width: 1024px) {
-        label {
-            font-size: 19px;
-        }
-    }
+    // @media screen and (min-width: 1024px) {
+    //     label {
+    //         font-size: 19px;
+    //     }
+    // }
 
-    @media screen and (min-width: 1200px) {
-        label {
-            font-size: 22px;
-        }
-    }
+    // @media screen and (min-width: 1200px) {
+    //     label {
+    //         font-size: 22px;
+    //     }
+    // }
     
-    `,
+    // `,
     Origin: styled.div `
        width: 100%;
        display:flex;
@@ -198,7 +214,7 @@ export const Styled = {
     }
     `,
     Function: styled.div `
-        width: 100%;
+       width: 100%;
        display:flex;
        flex-direction: column;
        color: ${mixins.colors.text};
@@ -225,8 +241,39 @@ export const Styled = {
     }
     `,
 
-    Buttons: styled.div `
+    Password_Container: styled.div `
+    display: flex;
+    flex-direction: column;
+    gap: 0.5rem;
+    color: ${mixins.colors.text};
+    font-size:${mixins.typograph.form};
+
+    @media screen and (min-width: 768px) {
+        label {
+            font-size: 16px;
+        }
+    }
+
+    @media screen and (min-width: 1024px) {
+        label {
+            font-size:19px;
+        }
+    }
+
+    @media screen and (min-width: 1200px) {
+        label {
+            font-size:22px;
+        }
+    }
+
+    input {
         width: 100%;
+    }
+
+    `,
+
+    Buttons: styled.div `
+        /* width: 100%; */
         display: flex;
         flex-direction: column;
         justify-content: center;
@@ -247,7 +294,13 @@ export const Styled = {
     }
 
     @media screen and (min-width: 1200px) {
+        display: flex;
+        flex-direction: row;
+        align-items: center;
         button {
+            position: relative;
+            left: 150px;
+            top: 35px;
             font-size: 22px;
             width: 21rem;
         }
