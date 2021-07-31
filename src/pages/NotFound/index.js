f652de537f9601cafb0a31f58d95c508bd92491a
Lucas Diniz <lucasalvarengadiniz@gmail.com>
2021-07-30 21:48:05 -0300

    fix-scrollview-bug

--- src/pages/NotFound/index.js
+++ src/pages/NotFound/index.js
@@ -1,19 +1,11 @@
 import React from 'react';
-import Footer from '../../components/Footer';
-import Logo from '../../assets/small-logo.png'
 
 import { Styled } from './styles';
 
 function NotFound() {
   return (
     <Styled.Container>
-      <Styled.Navbar>
-        <Styled.ContainerLogo to="/home">
-          <Styled.Logo src={Logo} />
-        </Styled.ContainerLogo>
-      </Styled.Navbar>
-      <Styled.ImgError/>
-      <Footer />
+      <Styled.ImgError />
     </Styled.Container>
   )
 }
