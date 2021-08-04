4f0c3cfc1668edcdc3503c9651d9946e00ee6d33
Caio Ribeiro <caio.ribeiro98@outlook.com>
2021-08-04 17:57:50 -0300

    Add Register Page

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,23 +6,27 @@ import Login from '../pages/Login';
 import Profile from '../pages/Profile';
 import Edit from '../pages/Profile/Edit'
 import Cart from '../pages/Cart';
+import Register from '../pages/Register';
 import NotFound from '../pages/NotFound';
 import { Styled } from './styles';
 import Navbar from '../components/NavBar';
 import Footer from '../components/Footer';
 
+
 export default function Routes() {
     const { auth } = useAuth();
     let isloginPage = useRouteMatch("/login");
+    let isRegisterPage = useRouteMatch("/register");
 
     return (
         <Styled.AppLayout>
-            {!isloginPage && <Navbar />}
-            <Styled.PageLayout loginPage={isloginPage}>
+            {!isloginPage || isRegisterPage && <Navbar />}
+            <Styled.PageLayout loginPage={isloginPage} registerPage={isRegisterPage}>
                 <Switch>
                     <Route path="/" exact component={Home} />
                     <Route path="/home" component={Home} />
                     <Route path="/cart" component={Cart} />
+                    <Route path="/register" component={Register} />
                     {auth ? <Route path="/profile" exact component={Profile} />  : ''}
                     {auth ? <Route path="/profile/edit" component={Edit} /> : ''}
                     <Route path="/login" component={Login} />
